ab994fdd38a0d49a49e9c98e6a5b7403b1bd2044
Matheus Leite de Castro <65515537+matheuslei@users.noreply.github.com>
2022-10-05 15:02:50 -0300

    Add files via upload

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,9 @@
+export const Login = () => {
+  return (
+    <div className="h-screen bg-red-700 text-white p-4 flex flex-col items-center space-y-6">
+      <header className=" container flex justify-center max-w-5xl p-4">
+        <img src="./logo/logo-fundo-vinho.svg" className="w-40" />
+      </header>
+    </div>
+  );
+};
